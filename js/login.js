1f72e074d1dde5752275069a345d9d3348326843
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-17 17:34:53 +0600

    write the logic of toggle the peek eye icon to eye slash icon

--- js/login.js
+++ js/login.js
@@ -1,6 +1,22 @@
+// get the element
+const peekElement = document.getElementById("peek-password");
+
 // handler function:: submit button
 document
   .getElementById("submitBtn")
   .addEventListener("click", function (event) {
     event.preventDefault();
   });
+
+//   handler function:: toggle the peek password
+let isPeekActive = false;
+peekElement.addEventListener("click", function () {
+  // toggle condition
+  if (!isPeekActive) {
+    peekElement.innerHTML = `<i class="fa-regular fa-eye-slash"></i>`;
+    isPeekActive = true;
+  } else {
+    peekElement.innerHTML = `<i class="fa-regular fa-eye"></i>`;
+    isPeekActive = false;
+  }
+});
