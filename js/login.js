07e03507f55510a861e6d18848d13f9342e23d85
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-17 14:58:05 +0600

    adding buttons effect

--- js/login.js
+++ js/login.js
@@ -1,1 +1,6 @@
-console.log("Log-in script");
+// handler function:: submit button
+document
+  .getElementById("submitBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
