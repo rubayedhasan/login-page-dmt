dcb217c435b4dc61e8889d592a8bb49023e90c43
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-18 10:22:00 +0600

    write the functionality of save the user name and password on clicking remember me and load the user details when window load

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 // get the element
-const passwordField = document.getElementById("password");
-const peekElement = document.getElementById("peek-password");
+const userNameField = document.querySelector("#user-name");
+const passwordField = document.querySelector("#password");
+const peekElement = document.querySelector("#peek-password");
+const rememberLogField = document.querySelector("#log-checked");
 
 // handler function:: submit button
 document
@@ -25,4 +27,35 @@ peekElement.addEventListener("click", function () {
     // hide the password
     passwordField.type = "password";
   }
+});
+
+// handler function :: remember me functionality
+// part-1:: saving the user details on clicking remember me
+rememberLogField.addEventListener("click", function () {
+  const username = userNameField.value;
+  const password = passwordField.value;
+
+  if (rememberLogField.checked) {
+    window.localStorage.setItem("userName", username);
+    window.localStorage.setItem("password", password);
+  }
+});
+
+// part-2:: render the user details on loading the window
+window.addEventListener("load", function () {
+  const myUser = window.localStorage.getItem("userName");
+  const myPassword = window.localStorage.getItem("password");
+
+  // validation:: set the user details on the input field
+  if (myUser && myPassword) {
+    userNameField.value = myUser;
+    passwordField.value = myPassword;
+    rememberLogField.checked = true;
+    return;
+  }
+
+  // reset the user details on the input field
+  userNameField.value = "";
+  passwordField.value = "";
+  rememberLogField.checked = false;
 });
